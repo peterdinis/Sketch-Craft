55ad285991ae9ec65e2b7a6e860cde54f3d52983
Peter Dinis <peterdinis0@pobox.sk>
2024-03-16 18:54:06 +0100

    FOO

--- src/components/liveblocks/cursors/LiveCursor.tsx
+++ src/components/liveblocks/cursors/LiveCursor.tsx
@@ -1,9 +1,34 @@
-import { FC } from "react";
+import { FC, PointerEvent, useCallback } from 'react';
+import { useMyPresence, useOthers } from '../../../../liveblocks.config';
+import LiveCursors from './LiveCursors';
 
 const LiveCursor: FC = () => {
-    return (
-        <></>
-    )
-}
+    const others = useOthers();
+    const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+    const handlePointerMove = useCallback((event: PointerEvent) => {
+        event.preventDefault();
+
+        const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+        const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+        updateMyPresence({ cursor: { x, y } });
+    }, []);
+
+    const handlePointerLeave = useCallback((event: PointerEvent) => {
+        event.preventDefault();
+        updateMyPresence({ cursor: null, message: null });
+    }, []);
+
+    const handlePointerDown = useCallback((event: PointerEvent) => {
+        event.preventDefault();
+        const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+        const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+        updateMyPresence({ cursor: { x, y } });
+    }, []);
+
+    return <></>;
+};
 
 export default LiveCursor;
