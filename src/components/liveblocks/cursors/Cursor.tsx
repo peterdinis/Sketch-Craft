3bd6860388aecdb3f93e2cee900e98c6e16be179
Peter Dinis <peterdinis0@pobox.sk>
2024-03-16 15:44:39 +0100

    feat: Working on cursors

---
+++ src/components/liveblocks/cursors/Cursor.tsx
@@ -0,0 +1,31 @@
+import { FC } from "react";
+import CursorSVG from "./CursorSVG";
+
+type CusrorProps = {
+  color: string;
+  x: number;
+  y: number;
+  message?: string;
+};
+
+const Cursor: FC<CusrorProps> = ({ color, x, y, message }: CusrorProps) => (
+  <div
+    className='pointer-events-none absolute left-0 top-0'
+    style={{ transform: `translateX(${x}px) translateY(${y}px)` }}
+  >
+    <CursorSVG color={color} />
+
+    {message && (
+      <div
+        className='absolute left-2 top-5 rounded-3xl px-4 py-2'
+        style={{ backgroundColor: color, borderRadius: 20 }}
+      >
+        <p className='whitespace-nowrap text-sm leading-relaxed text-white'>
+          {message}
+        </p>
+      </div>
+    )}
+  </div>
+);
+
+export default Cursor;
