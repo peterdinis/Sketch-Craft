a119e90b9f9d1eb235b7e4b981294466bba09e7a
Peter Dinis <peterdinis0@pobox.sk>
2024-03-16 15:04:54 +0100

    FOO

---
+++ src/components/liveblocks/CollaborativeApp.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { FC } from "react"
+import { useOthers } from "../../../liveblocks.config"
+
+const CollaborativeApp: FC = () => {
+    const others = useOthers();
+    const userCount = others.length;
+
+    return (
+        <div>There are {userCount} other user(s) online</div>
+    )
+}
+
+export default CollaborativeApp;
