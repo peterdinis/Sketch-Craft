0011a5cd755bd55cde7afd666e212111624d02cf
Peter Dinis <peterdinis0@pobox.sk>
2024-03-27 18:15:06 +0100

    FOO

--- src/components/shared/Navigation.tsx
+++ src/components/shared/Navigation.tsx
@@ -1,9 +1,25 @@
-import { FC } from "react";
+import { FC, memo } from 'react';
+import { NavbarProps } from '@/types/navbarTypes';
+import ActiveUsers from '../liveblocks/users/ActiveUsers';
 
-const Navigation: FC = () => {
+const Navigation: FC<NavbarProps> = ({
+    activeElement,
+    imageInputRef,
+    handleImageUpload,
+    handleActiveElement,
+}) => {
     return (
-        <></>
-    )
-}
+        <>
+            <nav className='flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white'>
+                LOGO LATER
+                <ActiveUsers />
+            </nav>
+        </>
+    );
+};
 
-export default Navigation;
+export default memo(
+    Navigation,
+    (prevProps, nextProps) =>
+        prevProps.activeElement === nextProps.activeElement,
+);
